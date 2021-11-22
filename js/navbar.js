1126a460103938bb7b7ce7bf68fa75a2fba5ec01
Quanh0810 <nqunhanh35@gmail.com>
2021-11-22 15:42:31 +0700

    update 22/11

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,21 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction(){
-    var x = document.getElementsByClassName("main-menu__item");
+let n = document.getElementById("nav");
+// function myFunction(event){
+//     event.preventDefault();
+//     var x = document.querySelector(".main-menu_wrap");
+//     if(x.style.display === "block"){
+//         x.style.display = "none";
+//     }else{
+//         x.style.display = "block";
+//     }
+// }
+n.addEventListener('click', (event)=>{
+    event.preventDefault();
+    var x = document.querySelector(".main-menu_wrap");
     if(x.style.display === "block"){
         x.style.display = "none";
     }else{
         x.style.display = "block";
     }
-}
-
+    console.log(n);
+})
